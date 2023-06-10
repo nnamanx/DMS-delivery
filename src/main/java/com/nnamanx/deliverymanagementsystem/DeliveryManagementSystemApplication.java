43a03e436a0ab78f8a8a3af5aefc3ca9595d0b66
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-06-10 10:58:10 +0400

    "first commit"

---
+++ src/main/java/com/nnamanx/deliverymanagementsystem/DeliveryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nnamanx.deliverymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nnamanx/deliverymanagementsystem/DeliveryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnamanx.deliverymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
