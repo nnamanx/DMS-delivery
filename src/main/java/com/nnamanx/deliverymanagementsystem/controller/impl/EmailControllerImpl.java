79878932f357507b5edc01fe128bf1e8f3870b5d
lamankhudadatzada <lkhudadatzada@gmail.com>
2023-06-30 11:26:25 +0400

    ""

--- src/main/java/com/nnamanx/deliverymanagementsystem/controller/impl/EmailControllerImpl.java
+++ src/main/java/com/nnamanx/deliverymanagementsystem/controller/impl/EmailControllerImpl.java
@@ -2,7 +2,6 @@ package com.nnamanx.deliverymanagementsystem.controller.impl;
 
 import com.nnamanx.deliverymanagementsystem.controller.EmailController;
 import com.nnamanx.deliverymanagementsystem.model.Email;
-import com.nnamanx.deliverymanagementsystem.service.EmailService;
 import com.nnamanx.deliverymanagementsystem.service.Impl.EmailServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,12 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+
+
+
 public class EmailControllerImpl implements EmailController {
     private final EmailServiceImpl emailService;
-
 
     @Override
     public void sendMail(@RequestBody Email email) {
         emailService.sendMail(email);
     }
 }
+
